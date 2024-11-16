f1b8946a039463b4a8c51efc0b8f7711b2a3f7d8
Diego Mendez <d.mendez@correo.unimet.edu.ve>
2024-11-15 20:08:41 -0400

    Arreglos menores y react icons agregado

--- MallMart/Mallmartfront/src/components/Navbar.jsx
+++ MallMart/Mallmartfront/src/components/Navbar.jsx
@@ -5,14 +5,12 @@ const Navbar = () => {
   return (
     <div className='bg-azulmorado flex justify-between items-center p-4'>
       <div className='logo'>
-      <img src="/Logonaranja.png" alt="Logo" className='h-8' />
+        <img src="/Logonaranja.png" alt="Logo" className='h-8' />
       </div>
       <div className='flex items-center'>
-        <div className='user mr-4'>
-          <FaUser  className='text-white text-2xl' />
-          <div className='cart'>
+        <div className='user flex items-center mr-4'> {/* Added flex here */}
+          <FaUser  className='text-white text-xl mr-2' /> {/* Added margin to the right */}
           <FaShoppingCart className='text-white text-2xl' />
-        </div>
         </div>
       </div>
     </div>
