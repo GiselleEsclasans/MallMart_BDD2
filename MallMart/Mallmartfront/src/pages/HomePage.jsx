1a5e42046ceed097263a9c4fcb5e9e22e8979b0f
Diego Mendez <d.mendez@correo.unimet.edu.ve>
2024-11-17 20:03:01 -0400

    Carrousel a componente

--- MallMart/Mallmartfront/src/pages/HomePage.jsx
+++ MallMart/Mallmartfront/src/pages/HomePage.jsx
@@ -1,4 +1,5 @@
 import Registercard from '../components/registercard';
+import Carrousel from '../components/carrousel';
 import Logincard from '../components/logincard';
 import Productcard from '../components/productcard';
 import Profilecard from '../components/profilecard';
@@ -15,53 +16,9 @@ function HomePage() {
             <Profilecard />
             <Logincard />
             <Registercard />
-        
+            <Carrousel />
             
-            <div className='MallMart bg-gradient-to-t from-moradooscuro to-purple-950'>
-                <div id="indicators-carousel" className="relative w-full" data-carousel="static">
-                    <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
-                        <div className="hidden duration-700 ease-in-out" data-carousel-item="active">
-                                <img src={logo} className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."></img>
-                            </div>
-                            <div className="hidden duration-700 ease-in-out" data-carousel-item>
-                                <img src={logo} className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."></img>
-                            </div>
-                            <div className="hidden duration-700 ease-in-out" data-carousel-item>
-                                <img src={logo} className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."></img>
-                            </div>
-                            <div className="hidden duration-700 ease-in-out" data-carousel-item>
-                                <img src={logo} className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."></img>
-                            </div>
-                            <div className="hidden duration-700 ease-in-out" data-carousel-item>
-                                <img src={logo}className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."></img>
-                            </div>
-                        </div>
-                        <div className="absolute z-30 flex -translate-x-1/2 space-x-3 rtl:space-x-reverse bottom-5 left-1/2">
-                            <button type="button" className="w-3 h-3 rounded-full" aria-current="true" aria-label="Slide 1" data-carousel-slide-to="0"></button>
-                            <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
-                            <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
-                            <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 4" data-carousel-slide-to="3"></button>
-                            <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 5" data-carousel-slide-to="4"></button>
-                        </div>
-                      
-                        <button type="button" className="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
-                            <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                                <svg className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 1 1 5l4 4"/>
-                                </svg>
-                                <span className="sr-only">Previous</span>
-                            </span>
-                        </button>
-                        <button type="button" className="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
-                            <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                                <svg className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 9 4-4-4-4"/>
-                                </svg>
-                                <span className="sr-only">Next</span>
-                            </span>
-                        </button>
-                    </div>
-                </div>
+            
             <div className='R1 p-5'>
                 <span className="text-3xl font-bold text-gray-900 dark:text-moradooscuro p-5">Recomendación General 1</span>
                 <div className="flex flex-wrap">
