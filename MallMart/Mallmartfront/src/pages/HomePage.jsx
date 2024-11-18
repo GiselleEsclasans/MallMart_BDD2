110d6e54a0a6bbf74ac061fc1554f5c2ea658341
Diego Mendez <d.mendez@correo.unimet.edu.ve>
2024-11-17 20:10:00 -0400

    Miniproductcard a componente

--- MallMart/Mallmartfront/src/pages/HomePage.jsx
+++ MallMart/Mallmartfront/src/pages/HomePage.jsx
@@ -3,8 +3,11 @@ import Carrousel from '../components/carrousel';
 import Logincard from '../components/logincard';
 import Productcard from '../components/productcard';
 import Profilecard from '../components/profilecard';
+import Categoryfooter from '../components/categoryfooter';
+import React from 'react';
 import logo from '../img/foto.png';
 import { Link } from 'react-router-dom';
+import Miniproductcard from '../components/miniproductcard';
 
 
 function HomePage() {
@@ -24,43 +27,9 @@ function HomePage() {
                 <div className="flex flex-wrap">
                     
                     
-                    <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg  dark:bg-morarosa m-5 border-b-4 border-b-moradooscuro shadow-lg">
-                    <Link onClick={() => {scroll(0, 0)}} to="/productos">
-                        <a href="#">
-                            <img className="p-8 rounded-t-lg" src={logo} alt="product image" />
-                        </a>
-                        <div className="px-5 pb-5">
-                        
-                            <div className="flex items-center mt-2.5 mb-5">
-                                <h5 className="pr-5 text-xl font-semibold tracking-tight text-gray-900 dark:text-white">Producto</h5>
-                                    <div className="flex items-center space-x-1 rtl:space-x-reverse">
-                                        <svg className="w-4 h-4 text-moradooscuro drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                                            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
-                                        </svg>
-                                        <svg className="w-4 h-4 text-moradooscuro drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                                            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
-                                        </svg>
-                                        <svg className="w-4 h-4 text-moradooscuro drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                                            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
-                                        </svg>
-                                        <svg className="w-4 h-4 text-moradooscuro drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                                            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
-                                        </svg>
-                                        <svg className="w-4 h-4 text-moradooscuro dark:text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                                            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
-                                        </svg>
-                                    </div>
-                                    <span className="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-moradooscuro dark:text-white ms-3">5.0</span>
-                                </div>
-                                <div className="flex items-center justify-between">
-                                    <span className="text-3xl font-bold text-gray-900 dark:text-white">$599</span>
-                                    <Link onClick={() => {scroll(0, 0)}} to="/carrito">
-                                        <a href="#" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-moradooscuro dark:hover:bg-moradoclaro dark:focus:bg-moradoclaro">Add to cart</a>
-                                    </Link>
-                                </div>
-                            </div>
-                            </Link>
-                    </div>
+                    <Miniproductcard />
+                    
+
                    
                     
                     <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg  dark:bg-morarosa m-5 border-b-4 border-b-moradooscuro shadow-lg">
@@ -194,16 +163,7 @@ function HomePage() {
                 </div>
                 
             </div>
-            <div className='Category bg-naranjaunimet p-5 pl-2'>
-            <span className="text-3xl font-bold text-gray-900 dark:text-white m-5">Categorías</span>
-                <div className='flex flex-nowrap justify-between'>
-                    <Link onClick={() => {scroll(0, 0)}} to="/productos" className='border-8 border-white  text-orange-950 font-bold py-4 px-4 size-24 rounded-full m-1'></Link>
-                    <Link onClick={() => {scroll(0, 0)}} to="/productos" className='border-8 border-white  text-orange-950 font-bold py-4 px-4 size-24 rounded-full m-1'></Link>
-                    <Link onClick={() => {scroll(0, 0)}} to="/productos" className='border-8 border-white  text-orange-950 font-bold py-4 px-4 size-24 rounded-full m-1'></Link>
-                    <Link onClick={() => {scroll(0, 0)}} to="/productos" className='border-8 border-white  text-orange-950 font-bold py-4 px-4 size-24 rounded-full m-1'></Link>
-                    <Link onClick={() => {scroll(0, 0)}} to="/productos" className='border-8 border-white  text-orange-950 font-bold py-4 px-4 size-24 rounded-full m-1'></Link>
-                </div>
-            </div>
+            <Categoryfooter />
         </div>
     );
 }
